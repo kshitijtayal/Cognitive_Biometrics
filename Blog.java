4c8c207e5625995d7516f288c8c88ff385da3643
kshitijtayal <kshitijtayal@gmail.com>
2015-05-20 11:54:21 -0400

    Blog.java

---
+++ Blog.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kshitijtayal
+ */
+public class Blog{
+
+String author_name;
+
+String desc;
+
+
+
+@Override
+
+public String toString() {
+
+return "Author:"+author_name+"\n"+"Desc:"+desc+"\n";
+
+}
+
+}
